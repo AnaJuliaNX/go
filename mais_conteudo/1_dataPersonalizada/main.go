0245419b0e4fc27340907546cbe810ec95ef788d
AnaJuliaNX <anajulia.negrixavier@gmail.com>
2023-09-21 09:43:18 -0300

    Estudo sobre datas

--- mais_conteudo/1_dataPersonalizada/main.go
+++ mais_conteudo/1_dataPersonalizada/main.go
@@ -12,11 +12,4 @@ func main() {
 	fmt.Println("Formato padrão do Go:", now)
 	//Formatando como usamos aqui no Brasil
 	fmt.Println("\nFormato personalizado:", now.Format("02/01/2006, 03:04:05"))
-
-	//ATENÇÃO: A data de fora é sempre comparada com a de dentro
-	//Comparando se uma data veio antes da outra:
-	fmt.Println("Verificando se veio antes:", menosDias.Before(maisDias))
-	//Comparando se uma data é depois da outra
-	fmt.Println("Verificando se veio depois:", menosDias.After(maisDias))
-
 }
