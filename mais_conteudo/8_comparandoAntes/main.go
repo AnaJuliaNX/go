b2c6f6f2ca97e2c1b51bbd5c28d02a6bd1ff2387
AnaJuliaNX <anajulia.negrixavier@gmail.com>
2023-09-21 09:42:30 -0300

    Estudo sobre datas

--- mais_conteudo/8_comparandoAntes/main.go
+++ mais_conteudo/8_comparandoAntes/main.go
@@ -10,5 +10,9 @@ func main() {
 	primeiraData := time.Now()
 	segundaData := time.Date(2005, 03, 03, 23, 50, 0, 0, time.Local)
 
+	//Atenção: a data de fora é sempre comparada com a de dentro
+	//É como se perguntassem: a data de fora vem antes da data de dentro?
 	fmt.Println("A primeira data veio antes da segunda?", primeiraData.Before(segundaData))
+
+	//O resultado da comparação é um valor booleano, ou seja, true or false
 }
